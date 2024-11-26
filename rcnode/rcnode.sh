99a33c4f8fd8f0417defe282cc8c174d94f8fbf6
alfiTH <alex_alfi@hotmail.com>
2024-11-26 15:35:43 +0100

    update rcnode to use $ROBOCOMP

--- rcnode/rcnode.sh
+++ rcnode/rcnode.sh
@@ -1,3 +1,2 @@
-yaku rcnode > /dev/null 2>&1
-icebox --Ice.Config=/home/robocomp/robocomp/tools/rcnode/rcnode.conf
-
+sed "s|\$ROBOCOMP|$ROBOCOMP|g" $ROBOCOMP/tools/rcnode/rcnode.conf > /tmp/rcnode.conf
+icebox --Ice.Config=/tmp/rcnode.conf > /dev/null 2>&1
