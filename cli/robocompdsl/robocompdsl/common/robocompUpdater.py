222c6085783ecfc87c97d8e90c52fd2e26ff1a35
Alejandro Torrejon Harto <66807131+alfiTH@users.noreply.github.com>
2025-04-08 10:24:44 +0200

    fix bug on robocompUpdater.py

--- cli/robocompdsl/robocompdsl/common/robocompUpdater.py
+++ cli/robocompdsl/robocompdsl/common/robocompUpdater.py
@@ -9,7 +9,7 @@ from pathlib import Path
 def crear_copia_seguridad(directorio:str):
     """Crea una copia de seguridad comprimida del directorio completo"""
     fecha = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-    nombre_zip = f"{directorio.replace("/", "-")}_{fecha}.zip"
+    nombre_zip = f"{directorio.replace('/', '-')}_{fecha}.zip"
     
     print(f"Creando copia de seguridad: {nombre_zip}")
     
