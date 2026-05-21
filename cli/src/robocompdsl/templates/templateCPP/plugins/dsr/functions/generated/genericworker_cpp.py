60a91bab09c4959c040f5f502f8e570a4c846b05
alfiTH <alex_alfi@hotmail.com>
2026-05-21 13:20:12 +0200

    build(dsr-cpp): remove OSG dependency when DSR option is enabled

--- cli/src/robocompdsl/templates/templateCPP/plugins/dsr/functions/generated/genericworker_cpp.py
+++ cli/src/robocompdsl/templates/templateCPP/plugins/dsr/functions/generated/genericworker_cpp.py
@@ -70,7 +70,7 @@ std::shared_ptr<DSR::DSRViewer> GenericWorker::setupViewer(std::shared_ptr<DSR::
     // Estructura de datos para iterar las opciones (más limpio que muchos IFs)
     const std::vector<std::pair<std::string, opts>> options = {
         {"tree", opts::tree}, {"graph", opts::graph}, 
-        {"2d", opts::scene},  {"3d", opts::osg}
+        {"2d", opts::scene}
     };
 
     for (const auto& [suffix, flag] : options) {
